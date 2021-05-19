b7b0a0e314df22b6f82d942fddd2b056822a0036
PankajMasiwal18 <pankajmasiwal18@gmail.com>
2021-05-19 18:03:28 +0530

    controllers

--- ExpressJS Mongoose CURD/routes/userRoutes.js
+++ ExpressJS Mongoose CURD/routes/userRoutes.js
@@ -1,104 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-var { user_Model } = require('../models/user_Model');
+const userController = require('../controllers/userController');
 
 
-router.get('/', async (req, res) => {
-    try{
-        const result = await user_Model.find();
-        res.status(200).send(result);
-    }
-    catch(err)
-    {
-        res.status(500).send(err);
-    }
-})
-
-router.get('/lookup', async (req, res) => {
-    try
-    {
-        await user_Model.aggregate([
-            { 
-                $lookup: 
-                { 
-                    from: "emp_details",
-                    localField: "user_id",  
-                    foreignField: "Emp_id", 
-                    as: "user_emp"
-                } 
-            }
-         ])
-    }
-    catch(err)
-    {
-        res.status(500).json({ error: err })
-    }
-})
-
-
-router.get('/:id', async (req, res) => {
-    var id = req.params.id;
-
-    try
-    {
-        const  result = await user_Model.findById(id);
-        res.status(200).send(result);
-    }
-    catch(err) 
-    {
-        res.status(500).send(err)
-    }
-})
-
-
-router.post('/', async (req, res) => {
-    var userData = new user_Model({
-        Name: req.body.name,
-        City: req.body.city,
-        Age: req.body.age,
-        user_id: req.body.user_id,
-    })
-    try
-    {
-        const user = await userData.save();
-        res.status(201).json({ message: "success" });
-    }
-    catch(err)
-    {
-        res.status(500).json({ err })
-    }
-})
-
-router.put('/:id', async (req,res)=>{
-  
-    var userData = {
-        Name: req.body.name,
-        City: req.body.city,
-        Age: req.body.age,
-        user_id: req.body.user_id
-    }
-    try
-    {
-        const updatedData = await user_Model.findByIdAndUpdate({_id: req.params.id},{ $set: userData },{new:true});
-        res.status(200).send(updatedData);
-    }
-    catch(err){
-        res.status(500).send(err)
-    }
-})
-
-router.delete('/:id', async (req,res)=>{
-    try
-    {
-        await user_Model.findByIdAndDelete(req.params.id);
-        res.status(200).send("Successfully Deleted");
-    }
-    catch(err)
-    {
-        res.status(500).send(err);
-    }
-})
+router.get('/', userController.all_User_Detail)
+router.get('/:id', userController.user_Id_Detail)
+router.post('/', userController.all_User_Detail)
+router.put('/:id', userController.update_User_Detail)
+router.delete('/:id', userController.delete_User_Detail)
+// aggregate (Lookup)
+router.get('/lookup', userController.lookup)
 
 
 module.exports = router;
